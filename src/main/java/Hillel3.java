d58d92401de4cd5eb2fb5917691bdfd66d4ba9d7
Ivan <Lihoy92ivan@gmail.com>
2017-05-23 10:42:44 +0300

    homework3

--- src/main/java/Hillel3.java
+++ src/main/java/Hillel3.java
@@ -2,12 +2,39 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 
 
+class Printer{
+
+    private void printRow(int rows){
+
+
+        String text = "##########################################";
+        int positionStart = 3;
+        int i = 0;
+        for (i = positionStart; i < rows + positionStart; i++){
+            System.out.println(text.subSequence(1, i) + " " + text.subSequence(1, i));
+        }
+    }
+
+    public void printTower(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Pleace enter row number");
+        int floors = scanner.nextInt();
+        while (floors <= 0 || floors > 1000 ){
+            System.out.println("Pleace enter correct number of the rows");
+            floors = scanner.nextInt();
+        }
+        System.out.println("Got correct number");
+        printRow(floors);
+    }
+
+}
+
 public class Hillel3 {
 
     public static void main(String[] args) {
-        stairCase();
-
-
+        //stairCase();
+        Printer printer = new Printer();
+        printer.printTower();
     }
 
     public static void stairCase() {
@@ -17,7 +44,6 @@ public class Hillel3 {
         for (int i = 1; i <= height; i++) {
 
             for (int j = 0; j <= height; j++) {
-
 
             if (height - i - 1 < j) {
                 System.out.print("#");
@@ -35,4 +61,5 @@ public class Hillel3 {
                 System.out.println();
         }
     }
+
 }
